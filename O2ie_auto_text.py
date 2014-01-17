2511967ed7d9e8af70f45566f60bf90345dfc23c
Sorcha Bowler <saoili@gmail.com>
2014-01-17 16:55:00 +0000

    removed login details

--- O2ie_auto_text.py
+++ O2ie_auto_text.py
@@ -52,12 +52,7 @@ def enter_message(browser,message):
 if __name__ == '__main__':
     
     browser = open_o2_in_browser()
-    loginDetails = {"0863799442": "Bhaa3799", "0863799601": "Bhaa9601",
-        "0863799591": "Bhaa9591", "0863799586": "Bhaa9586",
-        "0863799336": "Bhaa9336", "0863799459": "Bhaa9459",
-        "0863799507": "Bhaa9507", "0863799294": "Bhaa9294",
-        "0863799436": "Bhaa9436", "0863799240": "Bhaa9240"} 
-    #loginDetails = {"0863799591": "Bhaa9591"}
+    loginDetails = {"user1":"password 1", "user2": "password 2"}
     message = ask_for_message()
     for username in loginDetails:        
         password = loginDetails[username]
